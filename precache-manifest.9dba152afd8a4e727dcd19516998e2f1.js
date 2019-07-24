50b3997bce25fe127256f3db5c8e2fa752090f3d
Atick Faisal <atickfaisal@gmail.com>
2019-07-24 20:59:58 +0600

    Updates

---
+++ precache-manifest.9dba152afd8a4e727dcd19516998e2f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e57050c5b8dd1d3b8c393cbfba71a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b1841fe38fd188e335",
+    "url": "/static/css/main.fb0f2853.chunk.css"
+  },
+  {
+    "revision": "42d31b8b48188fabd34e",
+    "url": "/static/js/2.a09b9b6b.chunk.js"
+  },
+  {
+    "revision": "81b1841fe38fd188e335",
+    "url": "/static/js/main.00482645.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bfed69cea19a2b564d8d10313cc364e4",
+    "url": "/static/media/hum.bfed69ce.svg"
+  },
+  {
+    "revision": "2d989748cddee4dfa4a3e119c17d44e7",
+    "url": "/static/media/light.2d989748.svg"
+  },
+  {
+    "revision": "d10d3c5d353a81e76a1e6bd44111c3d6",
+    "url": "/static/media/temp.d10d3c5d.svg"
+  }
+]);
